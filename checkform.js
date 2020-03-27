708e7e89b6d040237ddd8dc554a7d7249d634cfe
Alexandra31150 <alexandrap44@gmail.com>
2020-03-27 13:56:58 +0100

    modif JS

--- checkform.js
+++ checkform.js
@@ -4,22 +4,23 @@
 // document.querySelector("form").addEventListener("submit", e => {
 //   check(e));
 // }
-
 const subBtn = document
   .getElementById("submitBtn")
   .addEventListener("click", check);
 function check(e) {
+  let checkCount = 0;
   const inputsCollection = document.querySelectorAll(
     "#contact input, #contact textarea"
   );
   console.log(inputsCollection);
   for (let inputToTest of inputsCollection) {
     if (inputToTest.value === "") {
+      checkCount++;
       e.preventDefault();
       alert(`Erreur : le champ ${inputToTest.name} n'a pas été rempli`);
     }
   }
+  if (checkCount === 0) {
+    alert(`Message envoyé !`);
+  }
 }
-$(".imgprofil").mouseleave(function() {
-  $(this).removeClass("imgprofil");
-});
